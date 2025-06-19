987dc2c63c8dd5a2a5b5956e9c9ff7c863de80dd
Sajin Maharjan <sajinmaharjan249@gmail.com>
2025-06-19 09:47:58 +0545

    add function to create grid with specific number.

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const container = document.querySelector('.container');
-
+const MAX_GRID_SIZE = 500;
 
 function createGrid(size){
-    for(let i = 0; i < 64; i++){
-        const div = document.createElement('div');
-        div.classList.add('grid-square');
-        container.appendChild(div);
+    let gridSize = MAX_GRID_SIZE / size;
+    let container = document.querySelector('.container');
+    let dimension = size * size;
+    for(let j = 0; j < dimension; j++){
+        let row = document.createElement('div');
+        row.classList.add('gridSquare');
+        row.style.height = `${gridSize}px`;
+        row.style.width = `${gridSize}px`;
+        container.appendChild(row);
     }
 }
 
-const gridSquare = document.querySelectorAll('.grid-square');
-gridSquare.forEach(function(singleDiv){
+createGrid(64); //default grid-size.
+
+document.querySelectorAll('.gridSquare').forEach(function(singleDiv){
     singleDiv.addEventListener('mouseover', (e) =>{
         e.target.style.backgroundColor = "black";
     })
@@ -24,8 +29,7 @@ document.querySelector('#input').addEventListener("click", (e)=>{
         if(!isNaN(numbers) && (!numbers < 0 && !numbers >= 100)){
             console.log(numbers);
         } else {
-            alert("Enter only from 0 to 100");
+            alert("Enter only number from 0 to 100");
         }
     }
 })
-
