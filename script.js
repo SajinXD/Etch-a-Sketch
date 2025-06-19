9d0053d1fbcd8a4984777594bd8b5698c058c6e3
Sajin Maharjan <sajinmaharjan249@gmail.com>
2025-06-19 13:26:35 +0545

    made some changes, user input from prompt works with grid size and working reset btn.

--- script.js
+++ script.js
@@ -3,33 +3,50 @@ const MAX_GRID_SIZE = 500;
 function createGrid(size){
     let gridSize = MAX_GRID_SIZE / size;
     let container = document.querySelector('.container');
+    container.innerHTML = "";
     let dimension = size * size;
     for(let j = 0; j < dimension; j++){
         let row = document.createElement('div');
         row.classList.add('gridSquare');
         row.style.height = `${gridSize}px`;
         row.style.width = `${gridSize}px`;
+        row.style.backgroundColor = "white";
         container.appendChild(row);
     }
-}
-
-createGrid(64); //default grid-size.
-
-document.querySelectorAll('.gridSquare').forEach(function(singleDiv){
+    document.querySelectorAll('.gridSquare').forEach(function(singleDiv){
     singleDiv.addEventListener('mouseover', (e) =>{
         e.target.style.backgroundColor = "black";
     })
 });
+}
 
-document.querySelector('#input').addEventListener("click", (e)=>{
-    let input = prompt("Select new grid from 0 to 100", 64);
+createGrid(64); //default grid-size.
+
+function getBackgroundColor(){
+
+}
+
+let resetBtn = document.querySelector('#reset');
+let inputBtn = document.querySelector('#input');
+let scolor = document.querySelector('#scolor');
+let rainbow = document.querySelector('#rainbow');
+
+
+resetBtn.addEventListener("click", (e)=>{
+    document.querySelectorAll('.gridSquare').forEach((grid)=>{
+        grid.style.backgroundColor = "white";
+    })
+})
+
+inputBtn.addEventListener("click", (e)=>{
+    let input = prompt("Select new grid from 1 to 100", 64);
     
     if(input !== null){
         let numbers = parseInt(input);
-        if(!isNaN(numbers) && (!numbers < 0 && !numbers >= 100)){
-            console.log(numbers);
+        if(!isNaN(numbers) && (numbers > 0 && numbers <= 100)){
+            createGrid(numbers);
         } else {
-            alert("Enter only number from 0 to 100");
+            alert("Enter only number from 1 to 100");
         }
     }
 })
